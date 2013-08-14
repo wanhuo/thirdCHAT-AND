5f87df4517fd12afabf8752d0fdafe21340d3c46
jervisliu <jervisliu@gmail.com>
2013-08-14 18:11:33 +0800

    add logout

---
+++ chat/src/com/easemob/demo/LogoutActivity.java
@@ -0,0 +1,52 @@
+package com.easemob.demo;
+
+import com.easemob.EaseMob;
+import com.easemob.chat.domain.Group;
+import com.easemob.demo.db.DBOpenHelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class LogoutActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.logout_actionsheet);
+	}
+
+	public void logout(View view) {
+        //clearn up global variables
+        MainActivity.allUsers.clear();
+        Group.allGroups.clear();
+        
+        //close contact db
+        DBOpenHelper.closeDB();
+
+        //reset password to null
+        Gl.setPassword(null);
+        //reset inited so that the new user can retrieve contact list after login 
+        Gl.setInited(false);
+        
+        EaseMob.logout();
+        finish();
+        MainActivity.instance.finish();
+//        setResult(RESULT_OK);
+		startActivity(new Intent(this, Login.class));
+		overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+	}
+
+	public void cancel(View view) {
+		finish();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		finish();
+		return true;
+	}
+
+}
