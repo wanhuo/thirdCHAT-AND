40cb87678d35559a6df1d80dd6b3943d3ae5eecc
Johnson Ma <xyma@yahoo.com>
2014-07-17 00:13:33 +0800

    remove the temp workaround for sdk.init been called multiple times, since sdk is using a pid file to detect this situation by itself

--- ChatDemoUI/src/com/easemob/chatuidemo/DemoApplication.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/DemoApplication.java
@@ -56,20 +56,6 @@ public class DemoApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-
-		int pid = android.os.Process.myPid();
-		String processAppName = getAppName(pid);
-		//如果使用到百度地图相关，这个if判断不能少
-		if (processAppName == null || processAppName.equals("")) {
-			// workaround for baidu location sdk 
-			// 百度定位sdk，定位服务运行在一个单独的进程，每次定位服务启动的时候，都会调用application::onCreate
-			// 创建新的进程。
-			// 但环信的sdk只需要在主进程中初始化一次。 这个特殊处理是，如果从pid 找不到对应的processInfo
-			// processName，
-			// 则此application::onCreate 是被service 调用的，直接返回
-			return;
-		}
-		
 		applicationContext = this;
 		instance = this;
 		// 初始化环信SDK,一定要先调用init()
