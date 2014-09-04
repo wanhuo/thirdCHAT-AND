394a180bc36f377dbf43cdcdaa51ec7037d4caab
youni <syyorient@gmail.com>
2014-09-04 11:22:34 +0800

    remove the process init check in applicaion onCreate funcition

--- ChatDemoUI/src/com/easemob/chatuidemo/DemoApplication.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/DemoApplication.java
@@ -61,18 +61,6 @@ public class DemoApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 
-		int pid = android.os.Process.myPid();
-		String processAppName = getAppName(pid);
-		// 如果使用到百度地图或者类似启动remote service的第三方库，这个if判断不能少
-		if (processAppName == null || processAppName.equals("")) {
-			// workaround for baidu location sdk
-			// 百度定位sdk，定位服务运行在一个单独的进程，每次定位服务启动的时候，都会调用application::onCreate
-			// 创建新的进程。
-			// 但环信的sdk只需要在主进程中初始化一次。 这个特殊处理是，如果从pid 找不到对应的processInfo
-			// processName，
-			// 则此application::onCreate 是被service 调用的，直接返回
-			return;
-		}
 		applicationContext = this;
 		instance = this;
 		EMChat.getInstance().setDebugMode(true);
