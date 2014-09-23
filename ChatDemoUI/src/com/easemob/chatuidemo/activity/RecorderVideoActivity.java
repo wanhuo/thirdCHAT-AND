f455e06f8eccd81ca50679304890869a3e7a08ab
zhanw <wei3292008@gmail.com>
2014-09-23 21:19:16 +0800

    fix sometimes recoder video crash

--- ChatDemoUI/src/com/easemob/chatuidemo/activity/RecorderVideoActivity.java
+++ ChatDemoUI/src/com/easemob/chatuidemo/activity/RecorderVideoActivity.java
@@ -1,3 +1,14 @@
+/************************************************************
+  *  * EaseMob CONFIDENTIAL 
+  * __________________ 
+  * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved. 
+  *  
+  * NOTICE: All information contained herein is, and remains 
+  * the property of EaseMob Technologies.
+  * Dissemination of this information or reproduction of this material 
+  * is strictly forbidden unless prior written permission is obtained
+  * from EaseMob Technologies.
+  */
 package com.easemob.chatuidemo.activity;
 
 import java.io.IOException;
@@ -40,10 +51,9 @@ import com.easemob.chatuidemo.video.util.Utils;
 import com.easemob.util.EMLog;
 import com.easemob.util.PathUtil;
 
-public class RecorderVideoActivity extends BaseActivity implements
-		OnClickListener, Callback, OnErrorListener, OnInfoListener {
+public class RecorderVideoActivity extends BaseActivity implements OnClickListener, Callback, OnErrorListener, OnInfoListener {
 
-	private final static String CLASS_LABEL="RecordActivity";
+	private final static String CLASS_LABEL = "RecordActivity";
 	private PowerManager.WakeLock mWakeLock;
 	private ImageView btnStart;// 开始录制按钮
 	private ImageView btnStop;// 停止录制按钮
@@ -53,33 +63,28 @@ public class RecorderVideoActivity extends BaseActivity implements
 	private SurfaceHolder surfaceHolder;
 	String localPath = "";// 录制的视频路径
 	private Camera mCamera;
-	//预览的宽高
-	private int previewWidth=480;
-	private int previewHeight=480;
-	
-	Parameters cameraParameters=null;
-	
+	// 预览的宽高
+	private int previewWidth = 480;
+	private int previewHeight = 480;
 
-	//分别为 默认摄像头（后置）、默认调用摄像头的分辨率、被选择的摄像头（前置或者后置）
-	int defaultCameraId = -1, defaultScreenResolution = -1 , cameraSelection = 0;
-	int defaultVideoFrameRate=-1;
-	
-	
+	Parameters cameraParameters = null;
+
+	// 分别为 默认摄像头（后置）、默认调用摄像头的分辨率、被选择的摄像头（前置或者后置）
+	int defaultCameraId = -1, defaultScreenResolution = -1, cameraSelection = 0;
+	int defaultVideoFrameRate = -1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题栏
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
 		// 选择支持半透明模式，在有surfaceview的activity中使用
 		getWindow().setFormat(PixelFormat.TRANSLUCENT);
 		setContentView(R.layout.recorder_activity);
-		PowerManager pm=(PowerManager) getSystemService(Context.POWER_SERVICE);
-		mWakeLock=pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, CLASS_LABEL);
+		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, CLASS_LABEL);
 		mWakeLock.acquire();
-		
-		
-		
+
 		btnStart = (ImageView) findViewById(R.id.recorder_start);
 		btnStop = (ImageView) findViewById(R.id.recorder_stop);
 		btnStart.setOnClickListener(this);
@@ -114,94 +119,74 @@ public class RecorderVideoActivity extends BaseActivity implements
 		if (mWakeLock == null) {
 			// 获取唤醒锁,保持屏幕常亮
 			PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-			mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK,
-					CLASS_LABEL);
+			mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, CLASS_LABEL);
 			mWakeLock.acquire();
 		}
 	}
-	
-	
-	private void handleSurfaceChanged(){
-		if(mCamera==null)
-		{
+
+	private void handleSurfaceChanged() {
+		if (mCamera == null) {
 			finish();
 			return;
 		}
-		
-		boolean hasSupportRate=false;
+
+		boolean hasSupportRate = false;
 		List<Integer> supportedPreviewFrameRates = mCamera.getParameters().getSupportedPreviewFrameRates();
-		if(supportedPreviewFrameRates!=null&&supportedPreviewFrameRates.size()>0)
-		{
+		if (supportedPreviewFrameRates != null && supportedPreviewFrameRates.size() > 0) {
 			Collections.sort(supportedPreviewFrameRates);
-			for(int i=0;i<supportedPreviewFrameRates.size();i++)
-			{
-				int supportRate=supportedPreviewFrameRates.get(i);
-				
-				if(supportRate==10)
-				{
-					hasSupportRate=true;
+			for (int i = 0; i < supportedPreviewFrameRates.size(); i++) {
+				int supportRate = supportedPreviewFrameRates.get(i);
+
+				if (supportRate == 10) {
+					hasSupportRate = true;
 				}
-				
+
 			}
-			if(hasSupportRate)
-			{
-				defaultVideoFrameRate=10;
-			}else{
-				defaultVideoFrameRate=supportedPreviewFrameRates.get(0);
+			if (hasSupportRate) {
+				defaultVideoFrameRate = 10;
+			} else {
+				defaultVideoFrameRate = supportedPreviewFrameRates.get(0);
 			}
-			
-			
-			
+
 		}
-		
-		System.out.println("supportedPreviewFrameRates"+supportedPreviewFrameRates);
-		
-		
-		//获取摄像头的所有支持的分辨率
-		List<Camera.Size> resolutionList=Utils.getResolutionList(mCamera);
-		if(resolutionList!=null&&resolutionList.size()>0)
-		{
-			Collections.sort(resolutionList,new Utils.ResolutionComparator());
-			Camera.Size previewSize=null;
-			if(defaultScreenResolution==-1)
-			{
-				boolean hasSize=false;
-				//如果摄像头支持640*480，那么强制设为640*480
-				for(int i=0;i<resolutionList.size();i++)
-				{
-					Size size=resolutionList.get(i);
-					if(size!=null&&size.width==640&&size.height==480)
-					{
-						previewSize=size;
-						previewWidth=previewSize.width;
-						previewHeight=previewSize.height;
-						hasSize=true;
+
+		System.out.println("supportedPreviewFrameRates" + supportedPreviewFrameRates);
+
+		// 获取摄像头的所有支持的分辨率
+		List<Camera.Size> resolutionList = Utils.getResolutionList(mCamera);
+		if (resolutionList != null && resolutionList.size() > 0) {
+			Collections.sort(resolutionList, new Utils.ResolutionComparator());
+			Camera.Size previewSize = null;
+			if (defaultScreenResolution == -1) {
+				boolean hasSize = false;
+				// 如果摄像头支持640*480，那么强制设为640*480
+				for (int i = 0; i < resolutionList.size(); i++) {
+					Size size = resolutionList.get(i);
+					if (size != null && size.width == 640 && size.height == 480) {
+						previewSize = size;
+						previewWidth = previewSize.width;
+						previewHeight = previewSize.height;
+						hasSize = true;
 						break;
 					}
 				}
-				//如果不支持设为中间的那个
-				if(!hasSize)
-				{
-					int mediumResolution=resolutionList.size()/2;
-					if(mediumResolution>=resolutionList.size())
-						mediumResolution=resolutionList.size()-1;
-					previewSize=resolutionList.get(mediumResolution);
-					previewWidth=previewSize.width;
-					previewHeight=previewSize.height;
-						
+				// 如果不支持设为中间的那个
+				if (!hasSize) {
+					int mediumResolution = resolutionList.size() / 2;
+					if (mediumResolution >= resolutionList.size())
+						mediumResolution = resolutionList.size() - 1;
+					previewSize = resolutionList.get(mediumResolution);
+					previewWidth = previewSize.width;
+					previewHeight = previewSize.height;
+
 				}
-				 
-			} 
-			 
+
+			}
+
 		}
-		
-		
-		
-		
-		
+
 	}
-	
-	 
+
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -210,8 +195,7 @@ public class RecorderVideoActivity extends BaseActivity implements
 			mWakeLock = null;
 		}
 	}
-	 
-	
+
 	@Override
 	public void onClick(View view) {
 		switch (view.getId()) {
@@ -230,14 +214,13 @@ public class RecorderVideoActivity extends BaseActivity implements
 			mediarecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
 			// 设置视频录制的分辨率。必须放在设置编码和格式的后面，否则报错
 			mediarecorder.setVideoSize(previewWidth, previewHeight);
-//			// 设置录制的视频帧率。必须放在设置编码和格式的后面，否则报错
+			// // 设置录制的视频帧率。必须放在设置编码和格式的后面，否则报错
 			if (defaultVideoFrameRate != -1) {
 				mediarecorder.setVideoFrameRate(defaultVideoFrameRate);
 			}
 			mediarecorder.setPreviewDisplay(surfaceHolder.getSurface());
 			// 设置视频文件输出的路径
-			localPath = PathUtil.getInstance().getVideoPath() + "/"
-					+ System.currentTimeMillis() + ".mp4";
+			localPath = PathUtil.getInstance().getVideoPath() + "/" + System.currentTimeMillis() + ".mp4";
 			mediarecorder.setOutputFile(localPath);
 			mediarecorder.setOnErrorListener(this);
 			mediarecorder.setOnInfoListener(this);
@@ -273,19 +256,15 @@ public class RecorderVideoActivity extends BaseActivity implements
 			btnStart.setVisibility(View.VISIBLE);
 			btnStop.setVisibility(View.INVISIBLE);
 
-			new AlertDialog.Builder(this)
-					.setMessage("是否发送？")
-					.setPositiveButton(R.string.ok,
-							new DialogInterface.OnClickListener() {
+			new AlertDialog.Builder(this).setMessage("是否发送？").setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 
-								@Override
-								public void onClick(DialogInterface arg0,
-										int arg1) {
-									arg0.dismiss();
-									sendVideo(null);
+				@Override
+				public void onClick(DialogInterface arg0, int arg1) {
+					arg0.dismiss();
+					sendVideo(null);
 
-								}
-							}).setNegativeButton(R.string.cancel, null).show();
+				}
+			}).setNegativeButton(R.string.cancel, null).show();
 
 			break;
 
@@ -295,8 +274,7 @@ public class RecorderVideoActivity extends BaseActivity implements
 	}
 
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		// 将holder，这个holder为开始在oncreat里面取得的holder，将它赋给surfaceHolder
 		surfaceHolder = holder;
 
@@ -306,7 +284,12 @@ public class RecorderVideoActivity extends BaseActivity implements
 	public void surfaceCreated(SurfaceHolder holder) {
 		// 将holder，这个holder为开始在oncreat里面取得的holder，将它赋给surfaceHolder
 		surfaceHolder = holder;
-		initpreview();
+		try {
+			initpreview();
+		} catch (Exception e) {
+			showFailDialog();
+			return;
+		}
 		handleSurfaceChanged();
 
 	}
@@ -321,56 +304,51 @@ public class RecorderVideoActivity extends BaseActivity implements
 	}
 
 	protected void releaseCamera() {
-		if (mCamera != null) {
-			mCamera.stopPreview();
-			mCamera.release();
-			mCamera = null;
+		try {
+			if (mCamera != null) {
+				mCamera.stopPreview();
+				mCamera.release();
+				mCamera = null;
+			}
+		} catch (Exception e) {
 		}
 	}
 
 	@SuppressLint("NewApi")
-	protected void initpreview() {
+	protected void initpreview() throws Exception {
 		try {
-			
-			if(Build.VERSION.SDK_INT>Build.VERSION_CODES.FROYO)
-			{
-				int numberOfCameras=Camera.getNumberOfCameras();
-				CameraInfo cameraInfo=new CameraInfo();
+
+			if (Build.VERSION.SDK_INT > Build.VERSION_CODES.FROYO) {
+				int numberOfCameras = Camera.getNumberOfCameras();
+				CameraInfo cameraInfo = new CameraInfo();
 				for (int i = 0; i < numberOfCameras; i++) {
 					Camera.getCameraInfo(i, cameraInfo);
-					if(cameraInfo.facing==cameraSelection)
-					{
-						defaultCameraId=i;
+					if (cameraInfo.facing == cameraSelection) {
+						defaultCameraId = i;
 					}
 				}
-				
-				
+
 			}
-			if(mCamera!=null)
-			{
+			if (mCamera != null) {
 				mCamera.stopPreview();
 			}
 
 			mCamera = Camera.open(CameraInfo.CAMERA_FACING_BACK);
 			mCamera.setPreviewDisplay(surfaceHolder);
-			setCameraDisplayOrientation(this, CameraInfo.CAMERA_FACING_BACK,
-					mCamera);
+			setCameraDisplayOrientation(this, CameraInfo.CAMERA_FACING_BACK, mCamera);
 			mCamera.startPreview();
 		} catch (Exception e) {
 			EMLog.e("###", e.getMessage());
-			showFailDialog();
-			return;
+			throw new Exception(e.getMessage());
 		}
 
 	}
 
 	@SuppressLint("NewApi")
-	public static void setCameraDisplayOrientation(Activity activity,
-			int cameraId, android.hardware.Camera camera) {
+	public static void setCameraDisplayOrientation(Activity activity, int cameraId, android.hardware.Camera camera) {
 		android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
 		android.hardware.Camera.getCameraInfo(cameraId, info);
-		int rotation = activity.getWindowManager().getDefaultDisplay()
-				.getRotation();
+		int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
 		int degrees = 0;
 		switch (rotation) {
 		case Surface.ROTATION_0:
@@ -405,22 +383,21 @@ public class RecorderVideoActivity extends BaseActivity implements
 			return;
 		}
 
-		msc = new MediaScannerConnection(this,
-				new MediaScannerConnectionClient() {
+		msc = new MediaScannerConnection(this, new MediaScannerConnectionClient() {
 
-					@Override
-					public void onScanCompleted(String path, Uri uri) {
-						System.out.println("scanner completed");
-						msc.disconnect();
-						setResult(RESULT_OK, getIntent().putExtra("uri", uri));
-						finish();
-					}
+			@Override
+			public void onScanCompleted(String path, Uri uri) {
+				System.out.println("scanner completed");
+				msc.disconnect();
+				setResult(RESULT_OK, getIntent().putExtra("uri", uri));
+				finish();
+			}
 
-					@Override
-					public void onMediaScannerConnected() {
-						msc.scanFile(localPath, "video/*");
-					}
-				});
+			@Override
+			public void onMediaScannerConnected() {
+				msc.scanFile(localPath, "video/*");
+			}
+		});
 		msc.connect();
 
 	}
@@ -441,38 +418,30 @@ public class RecorderVideoActivity extends BaseActivity implements
 	protected void onDestroy() {
 		super.onDestroy();
 		releaseCamera();
-		
-		if(mWakeLock!=null)
-		{
+
+		if (mWakeLock != null) {
 			mWakeLock.release();
-			mWakeLock=null;
+			mWakeLock = null;
 		}
-		
+
 	}
 
 	@Override
 	public void onBackPressed() {
 		back(null);
 	}
-	
-	
-	
-	
-	
-	private void showFailDialog(){
-		new AlertDialog.Builder(this).setTitle("提示").setMessage("打开设备失败！").setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-			
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				finish();
-				
-			}
-		}).setCancelable(false).show();
-		
-		
+
+	private void showFailDialog() {
+		new AlertDialog.Builder(this).setTitle("提示").setMessage("打开设备失败！")
+				.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+
+					}
+				}).setCancelable(false).show();
+
 	}
-	
-	 
-	
 
 }
